1f3cc1574ee5237be011b27e9f0f7affdab7f9a4
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-11-26 04:23:54 -0700

    Created account settings main page

---
+++ Frontend/rate_my_course/src/pages/AccountSettings.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+import '../styles/AccountSettings.css';
+
+import CustomButton from '../components/CustomButton';
+
+import AccountSettingsPassword from "./AccountSettingsPassword";
+import AccountSettingsMain from "./AccountSettingsAccount";
+
+
+const AccountSettings = () => {
+    // const [password, setPassword] = useState("")
+    // const [confirmPassword, setConfirmPassword] = useState("")
+    // const [confirmPassword2, setConfirmPassword2] = useState("")
+
+
+    // const [passwordError, setPasswordError] = useState("")
+
+    // const[showChangePasswordPage, setChangePasswordPage] = useState(false);
+    // const[showAccountPage, setAccountPage] = useState(false);
+
+    // const [activePage, setActivePage] = useState('account');
+
+    const [activePage, setActivePage] = useState('account');
+
+
+    const handleButtonClick = (page) => {
+        // setActivePage(page);
+        // document.getElementById(page).style.backgroundColor = "green";
+        setActivePage(page === activePage ? page : page);
+    };
+
+
+    
+
+    return (
+        <div className="mainContainer--accountSettings">
+            <div className="selectionsContainer">
+                <button className={activePage === "account" ? "active" : "selection"} onClick={() => handleButtonClick('account')}>
+                    Account
+                </button>
+                <button className={activePage === "password" ? "active" : "selection"} onClick={() => handleButtonClick('password')}>
+                    Password
+                </button>
+            </div>
+
+            <div className="componentContainer">
+                {activePage === 'password' && <AccountSettingsPassword/>}
+                {activePage === 'account' && <AccountSettingsMain/>}
+            </div>
+        </div>    
+    );
+    
+};
+
+export default AccountSettings;
