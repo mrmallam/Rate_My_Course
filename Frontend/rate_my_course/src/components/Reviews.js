89a25df9b8035431c92e57bdcc415e035d70bf40
Haris Ahmad <haris.ahmad1@ucalgary.ca>
2023-11-29 04:19:29 -0700

    added the thumbs up thing to the review box

--- Frontend/rate_my_course/src/components/Reviews.js
+++ Frontend/rate_my_course/src/components/Reviews.js
@@ -1,5 +1,7 @@
 import '../styles/Reviews.css';
 import React, { useState } from 'react';
+import thumbsUpGreen from '../resources/thumbs-up-green.svg';
+import thumbsUpBlank from '../resources/thumbs-up.svg';
 
 function RatingSet ({label, rating, setRating}) {
   const levels = [1,2,3,4,5];
@@ -25,6 +27,12 @@ function Reviews() {
     const [workload, setWorkload] = useState(5);
     const [usefulness, setUsefulness] = useState(5);
     const [comments, setComments] = useState('');
+    const [thumbsUpClicked, setThumbsUpClicked] = useState(false);
+
+    const handleThumbsUpClick = () => {
+      setThumbsUpClicked(!thumbsUpClicked);
+      //for back end logic
+    };
 
     return (
       <div className="container">    
@@ -53,11 +61,14 @@ function Reviews() {
             className='form-input flex-grow py-2 px-4 border-2 border-red-600 shadow-lg my-4 mx-5'
             placeholder='What do you want others to know about this class?'
           ></textarea>
-          <div className='ml-8 mt-2 mb-2 font-bold'>
-              <span className="text-md text-black">Upvote This Post?</span>
+          <div className='ml-8 mt-2 mb-2 font-bold flex'>
+            <div onClick={handleThumbsUpClick}>
+                <img src={thumbsUpClicked ? thumbsUpGreen : thumbsUpBlank} className="h-6 w-6 cursor-pointer" alt="thumbs-up"/>
+            </div>
+            <span className="text-md text-black ml-3">Upvote This Post?</span>
           </div>
           <div className='ml-8 mt-2 mb-2 font-bold'>
-              <span className="text-md text-gray-400">12 others found this post useful</span>
+            <span className="text-md text-gray-400">12 others found this post useful</span>
           </div>
 
 
