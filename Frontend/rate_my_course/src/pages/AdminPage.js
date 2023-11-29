fdf6b01431a10ed73646e96d039f05de46e84ddb
Haris Ahmad <haris.ahmad1@ucalgary.ca>
2023-11-29 01:04:30 -0700

    updated stuff

--- Frontend/rate_my_course/src/pages/AdminPage.js
+++ Frontend/rate_my_course/src/pages/AdminPage.js
@@ -5,12 +5,12 @@ import adminImage from '../resources/admin-image.svg';
 import editImage from '../resources/edit.svg';
 import deleteImage from '../resources/delete.svg';
 import thumbsUp from '../resources/thumbs-up-green.svg';
-import thumbsDown from '../resources/thumbs-down.svg';
+import thumbsDown from '../resources/thumbs-down-red.svg';
 import bookMark from '../resources/bookmark.svg';
 
 
 const MyReviews = () => {
-    const [activeTab, setActiveTab] = useState('myReviews');
+    const [activeTab, setActiveTab] = useState('reportedPosts');
     const [showPopup, setShowPopup] = useState(false);
     var name = "Jane Doe";
     var yearOfStudy = "3rd Year";
@@ -45,36 +45,35 @@ const MyReviews = () => {
                     <img src = {adminImage} className="admin-image w-24 h-24" alt="admin-image"/>
                 </div>
                 <div className="flex flex-col">
-                    <div className="user-name">
+                    <div className="user-name ml-2">
                         <p>{name}</p>
                     </div>
-                    <div className="user-year">
+                    <div className="user-year ml-2">
                         <p>{yearOfStudy}</p>
                     </div>
-                    <div className="user-course">
+                    <div className="user-course ml-2">
                         <p>{course} Student</p>
                     </div>
-                    <div className="user-university">
+                    <div className="user-university ml-2">
                         <p>{university}</p>
                     </div>
                 </div>
             </div>
             <div className='nav-bar'>
                 <div
-                    className={`nav-bar-item ${activeTab === 'myReviews' ? 'active' : ''}`}
-                    onClick={() => handleTabClick('myReviews')}>
+                    className={`nav-bar-item ${activeTab === 'reportedPosts' ? 'active' : ''}`}
+                    onClick={() => handleTabClick('reportedPosts')}>
                     <p>Reported Posts</p>
                 </div>
             </div>
 
             <div className='ml-8 mt-2 mb-6 font-bold w-1/6'>
-                <span className="text-md text-black">3 Reports Pending:</span>
+                <span className="text-md text-black">1 Report Pending:</span>
             </div>
 
             <div className="content">
-                {activeTab === 'myReviews' && (
+                {activeTab === 'reportedPosts' && (
                     <div className="review-content">
-                        <div className="reviews">
                             <div className="review">
                                 <div className="review-information">
                                     <div className="review-header">
@@ -90,74 +89,15 @@ const MyReviews = () => {
                                     </div>
                                 </div>
                                 <div className="modifications">
-                                    <div className="edit-button">
-                                        <img src = {editImage} className="icons" alt="edit-image"/>
+                                    <div className="edit-button flex">
+                                        <button className='h-10 w-1/8 bg-green-600 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline hover:bg-green-900'>Approve
+                                        </button>
                                     </div>
-                                    <div className="delete-button">
-                                        <img src = {deleteImage} className="icons" alt="delete-image"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-                {activeTab === 'ratedReviews' && (
-                    <div className="rated-reviews">
-                        <div className="reviews">
-                            <div className="review">
-                                <div className="review-information">
-                                    <div className="review-header">
-                                        <div className="review-course">
-                                            <p>SENG 513</p>
-                                        </div>
-                                        <div className="review-rating">
-                                            <p>5/5</p>
-                                        </div>
-                                    </div>
-                                    <div className="review-body">
-                                        <p>information</p>
+                                    <div className="delete-button flex">
+                                        <button className='h-10 w-1/8 bg-red-600 text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline hover:bg-red-900'>Remove
+                                        </button>                                    
                                     </div>
                                 </div>
-                                <div className="modifications">
-                                    <div className="edit-button">
-                                        <img src = {thumbsUp} className="icons" alt="thumbs-down"/>
-                                    </div>
-                                    <div className="delete-button">
-                                        <img src = {thumbsDown} className="thumbs-down" alt="delete-image"/>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                )}
-                {activeTab === 'watchedCourses' && (
-                    <div className="watched-courses">
-                        <div className="header">Your watched course</div>
-                        <div> Search Bar</div>
-                        <div className="courses">
-                            <div className="course">
-                                <div className="course-information">
-                                    <img src = {bookMark} className="bookmark" alt="bookmark"/>
-                                    SENG 513
-                                </div>
-                                <div className="reviews">
-                                    <div className="review">
-                                        <div className="review-information">
-                                            <div className="review-header">
-                                                <div className="review-course">
-                                                    <p>SENG 513</p>
-                                                </div>
-                                                <div className="review-rating">
-                                                    <p>5/5</p>
-                                                </div>
-                                            </div>
-                                            <div className="review-body">
-                                                <p>information</p>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 )}
