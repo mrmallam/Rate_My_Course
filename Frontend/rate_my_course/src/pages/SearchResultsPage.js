7d3cfb77cfe1745138900f2189779e93182f8ed9
Yianni Hontzias <47988330+Yianni8@users.noreply.github.com>
2023-12-01 14:47:49 -0700

    changes from meeting

--- Frontend/rate_my_course/src/pages/SearchResultsPage.js
+++ Frontend/rate_my_course/src/pages/SearchResultsPage.js
@@ -8,13 +8,13 @@ import '../styles/SearchResults.css';
 import React, { useState, useEffect, useCallback } from 'react';
 
 const courses = [
-    { id: 1, name: 'SENG 513', workload: 'High', difficulty: 'Hard' },
-    { id: 2, name: 'SENG 511', workload: 'Medium', difficulty: 'Low' },
-    { id: 3, name: 'SENG 501', workload: 'Low', difficulty: 'High' },
-    { id: 4, name: 'SENG 532', workload: 'High', difficulty: 'Medium' },
-    { id: 5, name: 'CPSC 532', workload: 'High', difficulty: 'Medium' },
-    { id: 6, name: 'CPSC 441', workload: 'High', difficulty: 'Medium' },
-    { id: 7, name: 'SENG 300', workload: 'High', difficulty: 'Low' },
+    { id: 1, name: 'SENG 513', workload: 'High', difficulty: 'Hard' , usefulness: 'Low'},
+    { id: 2, name: 'SENG 511', workload: 'Medium', difficulty: 'Low' , usefulness: 'Low'},
+    { id: 3, name: 'SENG 501', workload: 'Low', difficulty: 'High' , usefulness: 'Low'},
+    { id: 4, name: 'SENG 532', workload: 'High', difficulty: 'Medium' , usefulness: 'Low'},
+    { id: 5, name: 'CPSC 532', workload: 'High', difficulty: 'Medium' , usefulness: 'Low'},
+    { id: 6, name: 'CPSC 441', workload: 'High', difficulty: 'Medium' , usefulness: 'Low'},
+    { id: 7, name: 'SENG 300', workload: 'High', difficulty: 'Low' , usefulness: 'Low'},
     // this will be connected to  backend where all the search results will be stored
   ];
 
@@ -59,8 +59,12 @@ function Reviews() {
     return (
         <div className='search-container'>
             <div className='search-header'>
-                <h1 className="text-4xl">SENG 5xx</h1>
-                <img src={UniLogo} alt="University-Search-Logo" className='Logo'/>
+
+                {/* delete flex items-center to put logo back on top */}
+                <div className='flex items-center'>
+                    <img src={UniLogo} alt="University-Search-Logo" className='Logo'/>
+                    <h1 className="text-4xl mb-5">SENG 5xx</h1>
+                </div>
                 <div className='filters-container'>
 
                     <SearchComponent className='mb-4' data={courses} onSearchResults={handleSearchResults} />
@@ -68,7 +72,7 @@ function Reviews() {
                     <select 
                         value={courseCode} 
                         onChange={e => setCourseCode(e.target.value)}
-                        className='border-4 border-red-800 rounded-full p-3 mt-10'
+                        className='border-4 border-red-800 rounded-full p-3 mt-10 -ml-115'
                     >
                         <option value='----'>----</option>
                         <option value='SENG'>SENG</option>
@@ -90,7 +94,7 @@ function Reviews() {
                     <select 
                         value={filter} 
                         onChange={e => setFilter(e.target.value)}
-                        className='border-4 border-red-800 rounded-full p-3 lg:ml-5 sm:ml-0 mt-3'
+                        className='border-4 border-red-800 rounded-full p-3 lg:ml-5 -ml-15 mt-3'
                     >
                         <option value='-----'>-----</option>
                         <option value='Workload - Low to High'>Workload - Low to High</option>
@@ -110,6 +114,7 @@ function Reviews() {
                         <div className='result-right'>
                             <div className="text-2xl">Workload: {result.workload}</div>
                             <div className="text-2xl">Difficulty: {result.difficulty}</div>
+                            <div className="text-2xl">Usefulness: {result.usefulness}</div>
                         </div>
                     </div>
                 ))}
