b4d4c32dbe751141dcda0d0bc2ed8b46b7cccf89
Mohammed Allam <mohammed.allam@nutrien.com>
2023-12-02 16:41:52 -0700

    LandingPage & University Page Backend Integration

--- Backend/rateMyCourse/api/models.py
+++ Backend/rateMyCourse/api/models.py
@@ -6,7 +6,14 @@ class Persons(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
 
-class Universities(models.Model):
-    name = models.CharField(max_length=50)
-    reviews = models.IntegerField()
+class University(models.Model):
+    name = models.CharField(max_length=50, primary_key=True)
+    reviews = models.IntegerField(null=True, blank=True)
     # logo = models
+
+class Course(models.Model):
+    name = models.CharField(max_length=50)
+    university = models.ForeignKey(University, on_delete=models.CASCADE)
+    workload = models.CharField(max_length=25, default="Medium")
+    difficulty = models.CharField(max_length=25, default="Medium")
+    usefulness = models.CharField(max_length=25, default="Medium")
--- Backend/rateMyCourse/api/urls.py
+++ Backend/rateMyCourse/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PersonsViewSet, UserViewSet, UniversitiesViewSet
+from .views import PersonsViewSet, UserViewSet, UniversityViewSet, CourseViewSet
 
 
 router = DefaultRouter()
 router.register('persons', PersonsViewSet, basename='Persons')
-router.register('universities', UniversitiesViewSet, basename='Universities')
+router.register('University', UniversityViewSet, basename='University')
+router.register('Course', CourseViewSet, basename='Course')
 router.register('users', UserViewSet)
 
 urlpatterns = [
