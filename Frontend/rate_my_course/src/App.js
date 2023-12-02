d98a4a01848221cf2973c467f62cae93424cb3eb
Mohammed Allam <mohammed.allam@nutrien.com>
2023-12-01 17:24:23 -0700

    changes to Test

--- Frontend/rate_my_course/src/App.js
+++ Frontend/rate_my_course/src/App.js
@@ -9,15 +9,12 @@ import LandingPage from './pages/LandingPage.js';
 import SignupPage from './pages/SignupPage.js';
 import LoginPage from './pages/LoginPage.js';
 
-import AccountSettingsPassword from './pages/AccountSettingsPassword.js';
-import AccountSettingsMain from './pages/AccountSettingsMain.js';
 import MyProfile from './pages/MyProfle.js';
 
 import AdminPage from './pages/AdminPage.js';
 
 import ReviewPage from './pages/ReviewPage.js';
 import SearchResultsPage from './pages/SearchResultsPage.js';
-import AllCoursesUniversity from './pages/AllCoursesUniversity.js';
 import OverallCourseReviews from './pages/OverallCourseReviews.js';
 
 
@@ -26,20 +23,17 @@ function App() {
     <BrowserRouter>
       <Header/>
       <Routes>
-        {/* <LandingPage /> */}
+
         <Route exact path = "/home" Component={LandingPage}/>
         <Route exact path = "/signup" Component={SignupPage}/>
         <Route exact path = "/login" Component={LoginPage}/>
           
-        <Route exact path = "/AccountSettings/Main" Component={AccountSettingsPassword }/>
-        <Route path = "/AccountSettings/Main/Password" Component={AccountSettingsMain}/>
         <Route exact path = "/MyProfile" Component={MyProfile}/>
 
         <Route exact path = "/AdminPage" Component={AdminPage}/>
         
         <Route exact path = "/review" Component={ReviewPage}/>
         <Route exact path = "/results" Component={SearchResultsPage}/>
-        <Route exact path = "/allCourses" Component={AllCoursesUniversity}/>
         <Route exact path = "/overallCourseReview" Component={OverallCourseReviews}/>
       </Routes>
 
