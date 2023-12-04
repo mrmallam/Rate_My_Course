c1c661500c0a86e1ed82506908ee8f38bca8448b
Yianni Hontzias <47988330+Yianni8@users.noreply.github.com>
2023-12-04 00:32:41 -0700

    fix width of courses

--- Frontend/rate_my_course/src/components/CourseDiv.js
+++ Frontend/rate_my_course/src/components/CourseDiv.js
@@ -15,12 +15,13 @@ export default function Course_div({ data }) {
         return (
             <Link to={`/overallCourseReview/${encodeURIComponent(data.name)}`} className="w-3/4 md:w-4/12 h-24 mt-6">
                 <div className='search-results'>
-                    <div key={data.id} className='result-box border-2 border-red-600 container shadow-lg' onClick={() => courseChoice(data)}>
+                    <div key={data.id} className='result-box border-2 border-red-600 container shadow-lg w-full md:w-3/4 lg:w-1/2' onClick={() => courseChoice(data)}>
                         <div className='results-left'>
                             <h2 className="text-2xl lg:text-3xl">{data.name}</h2>
+                            <h2 className="text-1xl lg:text-1xl"> Computer Science class </h2>
                             <img src={Stars} alt="Starts-4" className='stars w-3/4' />
                         </div>
-                        <div className='result-right'>
+                        <div className='results-left'>
                             <div className="text-1xl lg:text-2xl whitespace-nowrap"><strong>Workload: </strong>2.4</div>
                             <div className="text-1xl lg:text-2xl whitespace-nowrap"><strong>Difficulty: </strong>3.4</div>
                             <div className="text-1xl lg:text-2xl whitespace-nowrap"><strong>Usefulness: </strong>4.8</div>
