44793598c43f2a780d323e6d339154015865a97f
Mohammed Allam <mohammed.allam@nutrien.com>
2023-12-06 15:19:25 -0700

    update

--- Frontend/rate_my_course/src/pages/LandingPage.js
+++ Frontend/rate_my_course/src/pages/LandingPage.js
@@ -44,9 +44,6 @@ useEffect(() => {
   }
 }, [isChecked, searchResults]); // Note: We use searchResults here, not displayResults
 
-
-  console.log(searchResults);
-
   return (
     <div className='flex flex-col items-center justify-center mb-5'>
       <Header />
