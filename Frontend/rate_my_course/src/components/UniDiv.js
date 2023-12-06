fe8033769423eab4d4d94b60134d43ff55a582c9
Anish Pokhrel <anishpokhrel2002@gmail.com>
2023-12-06 13:23:36 -0700

    Uni selection has hover effect

--- Frontend/rate_my_course/src/components/UniDiv.js
+++ Frontend/rate_my_course/src/components/UniDiv.js
@@ -7,7 +7,7 @@ export default function Uni_div({ data }) {
   if (data) {
     return (
       <Link to={`/UniversityPage/${encodeURIComponent(data.name)}`} className="w-3/4 md:w-4/12 h-24 mt-6">
-        <div className='border-2 border-red-600 container shadow-lg py-2 flex items-center justify-between'>
+        <div className='border-2 border-red-600 container shadow-lg py-2 flex items-center justify-between hover:scale-125 transition ease-in-out delay-0'>
           <img src={uni_logo} className="h-16 md:h-20 object-contain w-1/3 md:w-1/4" alt='logo' />
           <div className='w-2/3 md:w-3/4 px-2 md:pl-7'>
             <span className="block text-lg md:text-2xl font-normal text-left">{data.name}</span>
