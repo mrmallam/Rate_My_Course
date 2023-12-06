39b650fac7ac779ee285f672e2e145819e32140f
Mohammed Allam <mohammed.allam@nutrien.com>
2023-12-06 14:16:00 -0700

    fixes

--- Backend/rateMyCourse/api/urls.py
+++ Backend/rateMyCourse/api/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('api/', include(router.urls)),
 ]
 
-# # To display images in the browser during development
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# To display images in the browser during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
